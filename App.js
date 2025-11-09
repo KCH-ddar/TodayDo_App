6054f01530dac3bbca0b2709a5257cf7fb4b97c4
thsalrkd <6222mk@naver.com>
2025-11-09 22:20:01 +0900

    src 폴더 추가

--- App.js
+++ App.js
@@ -1,13 +1,9 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import SignUpEmailScreen from './screens/SignUp_1';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+  return <SignUpEmailScreen />;
 }
 
 const styles = StyleSheet.create({
